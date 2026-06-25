70026431659f27fe4d92ac0f5555a9a8057e280a
Carlos <carlosgualsaqui2005@gmail.com>
2026-06-24 19:44:49 -0500

    Initial commit

---
+++ src/main/java/com/itsqmet/talle2_backend/Talle2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.talle2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talle2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Talle2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsqmet/talle2_backend/Talle2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.talle2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Talle2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
